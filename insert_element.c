e4786068a49e9cf8e7a4f00bf50083984d1862f5
Tarun Kushwah <tarun@Taruns-MacBook-Air.local>
2026-08-06 21:34:01 +0530

    Insert element in given position in array

---
+++ insert_element.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10] = {10, 20, 100, 40, 50}; 
+    int value,pos;
+    printf("Enter value to insert an element \n");
+    scanf("%d",&value);
+    printf("Enter Position to insert an element \n");
+    scanf("%d",&pos);
+    for(int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+   for(int i=9;i>pos;i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    arr[pos]=value;
+    printf("\nchanged array");
+    for(int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
